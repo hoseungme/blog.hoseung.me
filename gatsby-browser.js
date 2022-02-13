c2e9071a9054daaee24d71d10be365e624f108b6
HoseungJang <hsjang.dev@gmail.com>
2022-02-14 00:01:10 +0900

    add root style

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,4 @@
-// custom typefaces
-import "typeface-montserrat";
-import "typeface-merriweather";
-// normalize CSS across browsers
-import "./src/normalize.css";
-// custom CSS styles
-import "./src/style.css";
+import "./src/styles/root.scss";
 
 // Highlighting for code blocks
 import "prismjs/themes/prism.css";
--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,7 @@ module.exports = {
     },
   },
   plugins: [
+    `gatsby-plugin-sass`,
     `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
