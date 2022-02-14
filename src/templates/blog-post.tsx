46638b6b55908e7bcd1c783999620626686ba068
HoseungJang <hsjang.dev@gmail.com>
2022-02-14 22:47:41 +0900

    fix blog-post, add blog-post style

--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -1,13 +1,14 @@
 import * as React from "react";
 import { Link, graphql } from "gatsby";
+import dayjs from "dayjs";
 
-import Bio from "../components/bio";
 import Layout from "../components/layout";
 import Seo from "../components/seo";
 
+import "../styles/templates/blog-post.scss";
+
 const BlogPostTemplate = ({ data, location }) => {
   const post = data.markdownRemark;
-  const { previous, next } = data;
 
   return (
     <Layout location={location}>
@@ -15,40 +16,10 @@ const BlogPostTemplate = ({ data, location }) => {
       <article className="blog-post" itemScope itemType="http://schema.org/Article">
         <header>
           <h1 itemProp="headline">{post.frontmatter.title}</h1>
-          <p>{post.frontmatter.date}</p>
+          <p>{dayjs(post.frontmatter.date).format("YYYY년 M월 D일")}</p>
         </header>
         <section dangerouslySetInnerHTML={{ __html: post.html }} itemProp="articleBody" />
-        <hr />
-        <footer>
-          <Bio />
-        </footer>
       </article>
-      <nav className="blog-post-nav">
-        <ul
-          style={{
-            display: `flex`,
-            flexWrap: `wrap`,
-            justifyContent: `space-between`,
-            listStyle: `none`,
-            padding: 0,
-          }}
-        >
-          <li>
-            {previous && (
-              <Link to={previous.fields.slug} rel="prev">
-                ← {previous.frontmatter.title}
-              </Link>
-            )}
-          </li>
-          <li>
-            {next && (
-              <Link to={next.fields.slug} rel="next">
-                {next.frontmatter.title} →
-              </Link>
-            )}
-          </li>
-        </ul>
-      </nav>
     </Layout>
   );
 };
