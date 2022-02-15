4a7901ef146999de52dbff123dc4d19c6a41818e
HoseungJang <hsjang.dev@gmail.com>
2022-02-16 02:26:21 +0900

    fix 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,14 +1,20 @@
 import * as React from "react";
+import { Link } from "gatsby";
 
 import Layout from "../components/layout";
 import Seo from "../components/seo";
 
+import "../styles/pages/404.scss";
+
 const NotFoundPage = () => {
   return (
     <Layout>
-      <Seo title="404: Not Found" />
-      <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <Seo title="Not Found" />
+      <div className="error-404-wrapper">
+        <img className="image" src="/404.png" alt="404 NOT FOUND" />
+        <p className="message">아무 것도 없어요..</p>
+        <Link className="link-to-home" to="/">홈으로</Link>
+      </div>
     </Layout>
   );
 };
