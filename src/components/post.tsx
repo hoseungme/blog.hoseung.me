dad72c6870caf330e3f90ea0b73c7f6781979b24
HoseungJang <hsjang.dev@gmail.com>
2022-02-16 00:14:54 +0900

    fix post

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -3,22 +3,28 @@ import { Link } from "gatsby";
 
 import { Post } from "../models/post";
 
-import "../styles/pages/index.scss";
+import "../styles/components/post.scss";
 
-export default function (post: Post) {
+export default function ({ posts }: { posts: Post[] }) {
   return (
-    <Link to={post.url} itemProp="url">
-      <article className="post-container" itemScope itemType="http://schema.org/Article">
-        <header>
-          <h2 className="title">
-            <span itemProp="headline">{post.title}</span>
-          </h2>
-          <p className="published-at">{post.publishedAt}</p>
-        </header>
-        <section>
-          <p className="description">{post.description}</p>
-        </section>
-      </article>
-    </Link>
+    <ul className="post-list">
+      {posts.map((post) => (
+        <li key={post.url} className="post-list-item">
+          <Link to={post.url} itemProp="url">
+            <article itemScope itemType="http://schema.org/Article">
+              <header>
+                <h2 className="title">
+                  <span itemProp="headline">{post.title}</span>
+                </h2>
+                <p className="published-at">{post.publishedAt}</p>
+              </header>
+              <section>
+                <p className="description">{post.description}</p>
+              </section>
+            </article>
+          </Link>
+        </li>
+      ))}
+    </ul>
   );
 }
