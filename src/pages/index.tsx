dc6971c122e55d9b4f3fb4a5d5893dd74faed468
HoseungJang <hsjang.dev@gmail.com>
2022-02-15 22:50:49 +0900

    fix index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,31 @@
 import React from "react";
 import { graphql } from "gatsby";
 
+import { Post } from "../models/post";
+
 import Layout from "../components/layout";
 import Seo from "../components/seo";
-import Post from "../components/post";
+import PostCard from "../components/post";
 
 import "../styles/pages/index.scss";
 
 export default function ({ data, location }: { data: QueryResult; location: any }) {
-  const posts = data.allMarkdownRemark.nodes;
+  const posts = React.useMemo<Post[]>(() => {
+    return data.allMarkdownRemark.nodes.map((node) => ({
+      title: node.frontmatter.title,
+      description: node.frontmatter.description,
+      tags: node.frontmatter.tags,
+      url: node.fields.slug,
+      publishedAt: node.frontmatter.date,
+    }));
+  }, [data]);
   return (
     <Layout location={location}>
       <Seo title="Home | blog.hoseung.me" />
       <ul className="post-list">
         {posts.map((post) => (
-          <li key={post.fields.slug} className="post-list-item">
-            <Post
-              title={post.frontmatter.title}
-              description={post.frontmatter.description}
-              tags={post.frontmatter.tags}
-              url={post.fields.slug}
-              publishedAt={post.frontmatter.date}
-            />
+          <li key={post.url} className="post-list-item">
+            <PostCard {...post} />
           </li>
         ))}
       </ul>
