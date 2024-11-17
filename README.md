0d667f1093a027619ff2f1c6df3b0de39528fa29
hoseungme <hsjang.dev@gmail.com>
2024-11-18 00:00:53 +0900

    remove gatsby legacy

--- README.md
+++
@@ -1,4 +0,0 @@
-# blog.hoseung.me
-
-- developed by Gatsby, deployed by Netlify
-- initialized by [gatsby-starter-blog](https://www.gatsbyjs.com/starters/gatsbyjs/gatsby-starter-blog)
--- src/utils/path.ts
+++
@@ -1,5 +0,0 @@
-import { Locale } from "../models/locale";
-
-export function path(pathname: string, locale: Locale) {
-  return locale === "ko" ? pathname : `/${locale}${pathname}`;
-}
