7e58f578efd3dca5645ed2c534cb23f477581a55
hoseungme <hsjang.dev@gmail.com>
2024-11-17 21:34:24 +0900

    add post utils

---
+++ blog/src/models/post.ts
@@ -0,0 +1,12 @@
+import { Locale } from "./locale";
+import { Tag } from "./tag";
+
+export interface Post {
+  id: string;
+  title: string;
+  description: string;
+  content: string;
+  publishedAt: number;
+}
+
+export type PostSummary = Omit<Post, "content">;
---
+++ blog/src/utils/post.ts
@@ -0,0 +1,56 @@
+import { readFileSync, readdirSync } from "fs";
+import { join } from "path";
+import { Post, PostSummary } from "@/models/post";
+
+export function getPosts(): PostSummary[] {
+  const dir = readdirSync(join(process.cwd(), "src/posts"));
+  const posts = dir.filter((file) => !file.startsWith(".")).sort((a, b) => (a < b ? 1 : -1));
+  return posts.map(getPostSummary);
+}
+
+function getPostSummary(id: string): PostSummary {
+  const post = getPost(id);
+  return {
+    id: post.id,
+    title: post.title,
+    description: post.description,
+    publishedAt: post.publishedAt,
+  };
+}
+
+export function getPost(id: string): Post {
+  const file = readFileSync(join(process.cwd(), "src/posts", id, "index.md")).toString();
+  const { metadata, content } = parsePost(file);
+
+  return {
+    id,
+    title: metadata.title,
+    description: metadata.title,
+    content,
+    publishedAt: new Date(metadata.date).valueOf(),
+  };
+}
+
+interface Metadata {
+  title: string;
+  description: string;
+  date: string;
+}
+
+function parsePost(fileContent: string) {
+  let frontmatterRegex = /---\s*([\s\S]*?)\s*---/;
+  let match = frontmatterRegex.exec(fileContent);
+  let frontMatterBlock = match![1];
+  let content = fileContent.replace(frontmatterRegex, "").trim();
+  let frontMatterLines = frontMatterBlock.trim().split("\n");
+  let metadata: Partial<Metadata> = {};
+
+  frontMatterLines.forEach((line) => {
+    let [key, ...valueArr] = line.split(": ");
+    let value = valueArr.join(": ").trim();
+    value = value.replace(/^['"](.*)['"]$/, "$1");
+    metadata[key.trim() as keyof Metadata] = value;
+  });
+
+  return { metadata: metadata as Metadata, content };
+}
