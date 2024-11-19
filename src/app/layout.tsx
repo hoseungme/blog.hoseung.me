9e51a840873a747112a6b76d9926aa590ff24a3d
hoseungme <hsjang.dev@gmail.com>
2024-11-20 00:52:26 +0900

    add gtag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,14 @@ import "./style.scss";
 import { ReactNode } from "react";
 import { Layout } from "./content/Layout";
 
+const GAScript = `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-EK99NNHV7V');
+`;
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -9,6 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-EK99NNHV7V"></script>
+        <script>{GAScript}</script>
+      </head>
       <body className="antialiased">
         <Layout>{children}</Layout>
       </body>
