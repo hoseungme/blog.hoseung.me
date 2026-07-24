6c2fac38ce2146c6fe8e0981553a547b23c84c46
hoseungme <hsjang.dev@gmail.com>
2026-07-25 06:44:53 +0900

    fix KST

--- src/app/[postId]/content/Page.tsx
+++ src/app/[postId]/content/Page.tsx
@@ -3,7 +3,7 @@ import "highlight.js/styles/github.css";
 import "./style.scss";
 
 import { Post } from "@/models/post";
-import { formatUTC } from "@/utils/date";
+import { formatKST } from "@/utils/date";
 import { differenceInYears } from "date-fns";
 import hljs from "highlight.js";
 import Markdown from "react-markdown";
@@ -19,7 +19,7 @@ export function Page({ post }: { post: Post }) {
         <h1 className="text-3xl font-bold mb-3" itemProp="headline">
           {post.title}
         </h1>
-        <p className="text-gray-500 font-mono">{formatUTC(post.publishedAt, "PP")}</p>
+        <p className="text-gray-500 font-mono">{formatKST(post.publishedAt, "PP")}</p>
       </header>
       <main className="my-8">
         <Markdown
--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,6 +1,6 @@
 import { TZDate } from "@date-fns/tz";
 import { format } from "date-fns";
 
-export function formatUTC(timestamp: number, formatString: string) {
-  return format(new TZDate(timestamp, "+00:00"), formatString);
+export function formatKST(timestamp: number, formatString: string) {
+  return format(new TZDate(timestamp, "+09:00"), formatString);
 }
